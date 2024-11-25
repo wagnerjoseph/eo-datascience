e42fe7d034f051c47288c29e686a93b18b80ec18
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2024-11-25 10:09:21 +0100

    fix quarto ordering (#7) and make kernel target (#6)

--- src/eo_datascience/clean_nb.py
+++ src/eo_datascience/clean_nb.py
@@ -3,7 +3,8 @@ import nbformat
 from pathlib import Path
 import re
 
-def clean_up_frontmatter(dir = './notebooks', save=True):
+
+def clean_up_frontmatter(dir="./notebooks", save=True):
     # Define the path to the notebooks
     nb_paths = find_ipynb(dir)
 
@@ -11,24 +12,24 @@ def clean_up_frontmatter(dir = './notebooks', save=True):
     for nb_path in nb_paths:
         # Load the notebook
         nb = nbformat.read(nb_path, as_version=4)
-        if nb.cells[0].source.startswith('---'):
-            #Load frontmatter
-            fm = nb.cells[0].source.split('\n')
+        if nb.cells[0].source.startswith("---"):
+            # Load frontmatter
+            fm = nb.cells[0].source.split("\n")
 
             # Extract the title and the subtitle and convert
             i = 1
             line = fm[i]
             new_text = []
             while not line.startswith("---"):
-                if line.startswith('title'):
+                if line.startswith("title"):
                     new_text.append(f"# {line.split(': ')[1]}")
-                if line.startswith('subtitle'):
+                if line.startswith("subtitle"):
                     new_text.append(f"**{line.split(': ')[1]}**")
 
                 i += 1
                 line = fm[i]
 
-            new_text += fm[i+1:]
+            new_text += fm[i + 1 :]
             nb.cells[0].source = "\n".join(new_text) + "\n"
         # Save notebook
         if save:
@@ -36,18 +37,22 @@ def clean_up_frontmatter(dir = './notebooks', save=True):
         else:
             return nb
 
-def convert_bibliography(nb_path="./notebooks/references.ipynb", save=True):
-    nb = nbformat.read(nb_path, as_version=4)
-    nb.cells[0].source = """# References
 
-```{bibliography}
-```
-"""
-     # Save the notebook
-    if save:
-        nbformat.write(nb, nb_path)
-    else:
-        return nb
+def convert_bibliography(nb_path=Path("./notebooks/references.ipynb"), save=True):
+    
+    if nb_path.exists():
+        nb = nbformat.read(nb_path, as_version=4)
+        nb.cells[0].source = """# References
+
+    ```{bibliography}
+    ```
+    """
+        # Save the notebook
+        if save:
+            nbformat.write(nb, nb_path)
+        else:
+            return nb
+
 
 def convert_callout_notes(dir="./notebooks", save=True):
     nb_paths = find_ipynb(dir)
@@ -57,27 +62,30 @@ def convert_callout_notes(dir="./notebooks", save=True):
         # Load the notebook
         nb = nbformat.read(nb_path, as_version=4)
         for i in range(len(nb.cells)):
-
             if nb.cells[i]["cell_type"] == "markdown":
                 nb.cells[i].source = quarto_note_replace(nb.cells[i].source)
+        if save:
+            nbformat.write(nb, nb_path)
+        else:
+            return nb
 
-     # Save the notebook
-    if save:
-        nbformat.write(nb, nb_path)
-    else:
-        return nb
-    
+
 def quarto_note_replace(quarto):
     note_rst_start = r":::{note}"
     note_rst_end = r":::"
     nts = re.findall(r"(?<=:::\s\{\.callout\-note\})[^:::]+", quarto)
     for i in nts:
-        quarto = re.sub(r":::\s\{\.callout\-note\}" + re.escape(i) + r":::", note_rst_start + i + note_rst_end, quarto)
+        quarto = re.sub(
+            r":::\s\{\.callout\-note\}" + re.escape(i) + r":::",
+            note_rst_start + i + note_rst_end,
+            quarto,
+        )
     return quarto
+
 
 def convert_refs(dir="./notebooks", save=True):
     nb_paths = find_ipynb(dir)
-    
+
     # Iterate over the notebooks
     for nb_path in nb_paths:
         # Load the notebook
@@ -88,6 +96,7 @@ def convert_refs(dir="./notebooks", save=True):
                     nb.cells[i].source = quarto_ref_figure_replace(nb.cells[i].source)
                     nb.cells[i].source = quarto_ref_person_replace(nb.cells[i].source)
                     nb.cells[i].source = quarto_ref_time_replace(nb.cells[i].source)
+            nb
 
         # Save the notebook
         if save:
@@ -95,17 +104,20 @@ def convert_refs(dir="./notebooks", save=True):
         else:
             return nb
 
+
 def quarto_ref_figure_replace(quarto):
     bibs = re.findall(r"(?<=\(\@)[^\)]+", quarto)
     for i in bibs:
-        quarto = re.sub(r"\(\@" + i + "\)", r"", quarto)
+        quarto = re.sub(r"\(\@" + i + r"\)", "", quarto)
     return quarto
+
 
 def quarto_ref_person_replace(quarto):
     bibs = re.findall(r"(?<=\[\@)[^\]]+", quarto)
     for i in bibs:
-        quarto = re.sub(r"\[\@" + i + "\]", r"{cite:p}`" + i + "`", quarto)
+        quarto = re.sub(r"\[\@" + i + r"\]", r"{cite:p}`" + i + "`", quarto)
     return quarto
+
 
 def quarto_ref_time_replace(quarto):
     bibs = re.findall(r"(?<=\@)[^\s]+", quarto)
@@ -113,10 +125,12 @@ def quarto_ref_time_replace(quarto):
         quarto = re.sub(r"\@" + i, r"{cite:t}`" + i + "`", quarto)
     return quarto
 
+
 def find_ipynb(dir):
     root = Path(dir).resolve()
-    nb_paths = [root / file for file in os.listdir(root) if file.endswith('.ipynb')]
+    nb_paths = [root / file for file in os.listdir(root) if file.endswith(".ipynb")]
     return nb_paths
+
 
 def main():
     clean_up_frontmatter()
@@ -125,5 +139,5 @@ def main():
     convert_bibliography()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
