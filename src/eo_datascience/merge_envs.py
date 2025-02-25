cdd3e74e173051806f3eaf179bb98f4e6b07e05c
npikall <nikolas.pikall@gmail.com>
2025-02-25 16:44:53 +0100

    refactored the merging of the environments

--- src/eo_datascience/merge_envs.py
+++ src/eo_datascience/merge_envs.py
@@ -1,52 +1,70 @@
-from pathlib import Path
-
 import yaml
+from pathlib import Path
 from packaging.version import parse
+from typing import List, Dict, Tuple, Set, Iterable
 
 
-def main():
-    root = Path("notebooks").resolve()
-    print(root, root.exists())
+def collect_yaml_files(root: Path) -> List[Path]:
     files = list(root.glob("**/*.yml"))
     files.append(root.parent / "environment.yml")
+    return files
 
-    dependencies = set()
-    fixed_dependencies = dict()
 
+def get_environment_from_yml(file: Path) -> Dict:
+    with file.open("r") as f:
+        environment = yaml.safe_load(f)
+    return environment
+
+
+def aggregate_env_dependencies(files: List[Path]) -> List[str]:
+    unrefined_dependencies: List = []
     for file in files:
-        with file.open("r") as f:
-            environment = yaml.safe_load(f)
+        environment = get_environment_from_yml(file)
+        unrefined_dependencies.append(environment.get("dependencies", []))
+    return unrefined_dependencies
 
-        for d in environment.get("dependencies", []):
-            parts = d.split("=")
-            name = parts[0]
 
-            if len(parts) > 1:
-                version = parts[-1]
-                if name in fixed_dependencies:
-                    fixed_dependencies[name].append(version)
-                else:
-                    fixed_dependencies[name] = [version]
+def extract_unique_dependencies(dep: List[str]) -> Tuple[Set, Dict]:
+    dependencies = set()
+    multi_versions = dict()
+    for d in dep:
+        parts = d.split("=")
+        name = parts[0]
+        dependencies.add(name)
+        # Check if version is specified
+        if len(parts) > 1:
+            version = parts[-1]
+            if name in multi_versions:
+                multi_versions[name].append(version)
+            else:
+                multi_versions[name] = [version]
+    return dependencies, multi_versions
 
-            dependencies.add(name)
 
-    # Update dependencies set with latest versions
+def resolve_dependency_versions(
+    unique_dependencies: Iterable, multi_versions: Dict
+) -> Set:
     final_dependencies = set()
-    for name in dependencies:
-        if name in fixed_dependencies:
-            latest_version = max(fixed_dependencies[name], key=parse)
+    for name in unique_dependencies:
+        if name in multi_versions:
+            latest_version = max(multi_versions[name], key=parse)
             final_dependencies.add(f"{name}={latest_version}")
         else:
             final_dependencies.add(name)
+    return final_dependencies
 
-    # Create master YAML file
+
+def create_master_environment(final_dependencies: List | Set) -> Dict:
     master_env = {
         "name": "eo-datascience",
         "channels": ["conda-forge"],
         "dependencies": sorted(final_dependencies),
     }
-    yaml_file = "environment.yml"
-    with open(yaml_file, "w") as f:
+    return master_env
+
+
+def dump_environment(output_file: str | Path, master_env: Dict) -> None:
+    with open(output_file, "w") as f:
         yaml.dump(
             master_env,
             f,
@@ -56,19 +74,42 @@ def main():
             width=80,
         )
 
-    # Dirty fix: Read the file and add two spaces before
-    with open(yaml_file, "r") as f:
+
+def fix_yml_indentation(output_file):
+    with open(output_file, "r") as f:
         lines = f.readlines()
 
-    with open(yaml_file, "w") as f:
+    with open(output_file, "w") as f:
         for line in lines:
             if line.strip().startswith("-"):
                 f.write("  " + line)  # Add two spaces before the line
             else:
                 f.write(line)
 
+
+def main(output_file: str | Path = "environment.yml") -> None:
+    root = Path("notebooks").resolve()
+    files = collect_yaml_files(root)
+
+    # Collect all dependencies from all YAML files
+    unrefined_dependencies = aggregate_env_dependencies(files)
+
+    unique_dependencies, multi_versions = extract_unique_dependencies(
+        unrefined_dependencies
+    )
+    # Update dependencies set with latest versions
+    final_dependencies = resolve_dependency_versions(
+        unique_dependencies, multi_versions
+    )
+
+    # Create master YAML file
+    master_env = create_master_environment(final_dependencies)
+    dump_environment(output_file, master_env)
+
+    # Dirty fix: Read the file and add two spaces before
+    fix_yml_indentation(output_file)
     print("Master environment file created successfully.")
 
 
 if __name__ == "__main__":
-    main()
+    main(output_file="environment.yml")
---
+++ tests/test_merge_env.py
@@ -0,0 +1,6 @@
+import pytest
+from pathlib import Path
+from eo_datascience import merge_envs
+
+def test_parse_dependencies_returns_correct():
+    raise NotImplementedError
